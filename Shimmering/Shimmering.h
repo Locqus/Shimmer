9fd0deff56976a610521c8b21274d322d53c53a4
Stefan Arambasich <ArtisOracle@gmail.com>
2016-09-01 12:53:42 -0400

    Adds carthage support

---
+++ Shimmering/Shimmering.h
@@ -0,0 +1,22 @@
+//
+//  Shimmering.h
+//  Shimmering
+//
+//  Created by Stefan Arambasich on 9/1/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shimmering.
+FOUNDATION_EXPORT double ShimmeringVersionNumber;
+
+//! Project version string for Shimmering.
+FOUNDATION_EXPORT const unsigned char ShimmeringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shimmering/PublicHeader.h>
+
+#import <Shimmering/FBShimmering.h>
+#import <Shimmering/FBShimmeringLayer.h>
+#import <Shimmering/FBShimmeringView.h>
+
